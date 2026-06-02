bac6a91b8c660e0947d2aa10443ef2e6b72ed590
Mohan <mohandhassgkvsb@gmail.com>
2026-06-02 21:36:14 +0530

    Resolved merge conflicts

--- Roug.java
+++
@@ -1,9 +0,0 @@
-HashMap<Integer>  num = new HashMap<>();
-int n = nums.length;
-Arrays.sort(nums);
-for(int i=0; i<n-1; i++){
-    if(nums[i] + nums[i+1] == tagret){
-        num.put(nums[i] , nums[i+1])
-    }
-    return num;
-}
